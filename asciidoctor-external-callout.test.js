07333563079b853980addb5bde7d2acc73351870
Ray Offiah <ray.offiah@couchbase.com>
2022-06-04 12:29:15 +0100

    Added Jest support

---
+++ asciidoctor-external-callout.test.js
@@ -0,0 +1,30 @@
+const asciidoctor = require('@asciidoctor/core')()
+
+require('./asciidoctor-external-callout')
+
+test('Load basic file', () => {
+
+    let input_document = ` 
+:source-highlighter: highlight.js
+:icons: font
+
+[source, shell]
+----
+#!/bin/bash
+
+# example of using arguments to a script
+echo "My first name is $1"
+echo "My surname is $2"
+echo "Total number of arguments is $#"
+----
+. Echo the surname @5
+. Explain the total number of arguments   @/number of .+/
+`
+    let converted_doc = asciidoctor.convert(input_document,{safe: 'safe', standalone: true})
+    expect(converted_doc.length).toBeGreaterThan(0)
+    expect(converted_doc.includes('My first name is $1')).toBeTruthy()
+    expect(converted_doc.includes('Echo the surname @5')).toBeFalsy()
+    expect(converted_doc.includes('My surname is $2" <i class="conum" data-value="1">')).toBeTruthy()
+    expect(converted_doc.includes('Total number of arguments is $#" <i class="conum" data-value="2">'))
+
+})
